f344ae3ca8d7a0962a0c24e8af312c7f9df5577b
eric095 <ericayewmensah@gmail.com>
2021-02-07 18:43:52 +0000

    added the answers to view and forgot to create a branch

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -28,4 +28,9 @@ class Answer extends Model
             echo "Answer saved\n";
         }); 
     }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -17,6 +17,52 @@
                 </div>
                 <div class="card-body">
                     {{ $question->body }}
+                    <div class="float-right">
+                        <span class="text-muted">Answered {{ $question->created_date }}<span>
+                        <div class="media mt-2">
+                            <a href="{{ $question->user->url }}" class="pr-2">
+                                <img src="{{ $question->url->avatar }}" />
+                            </a>
+                            <div class="media-body mt-1">
+                                <a href="{{ $question->user->url }}">
+                                    {{ $question->url->name }}
+                                </a> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>{{ $question->answers_count . " " . Str::plural('Answer', $question->answers_count) }}</h2>
+                    </div>
+                    <hr />
+                    @foreach($question->answers as $answer)
+                        <div class="media">
+                            <div class="media-body">
+                                {{ $answer->body }}
+                                <div class="float-right">
+                                    <span class="text-muted">Answered {{ $answer->created_date }}<span>
+                                    <div class="media mt-2">
+                                        <a href="{{ $answer->user->url }}" class="pr-2">
+                                            <img src="{{ $answer->url->avatar }}" />
+                                        </a>
+                                        <div class="media-body mt-1">
+                                            <a href="{{ $answer->user->url }}">
+                                                {{ $answer->url->name }}
+                                            </a> 
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr />
+                    @endforach
                 </div>
             </div>
         </div>
